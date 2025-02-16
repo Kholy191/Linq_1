464f56e6b58537a534db07fec441c2a0c001149b
Ahmed El-Kholy <a.adel27111@gmail.com>
2025-02-16 09:40:28 +0200

    Q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1 Find all products that are out of stock.
+
+            //var Result = ListGenerator.ProductList.Where(P => P.UnitsInStock == 0);
+
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+        }
+    }
+}
